083697a658a0c6f2821f49eca11e659072fc2ac0
Maksim <maxyarmoluk@gmail.com>
2026-05-02 22:09:45 +0300

    Первый коммит

---
+++ src/HW1.java
@@ -0,0 +1,7 @@
+public class HW1 {
+    private static String strOut = "Ярмолюк. 22.04.2026 19:46";
+
+    public void printResult(){
+        System.out.print(strOut);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    private static final HW1 hw1 = new HW1();   //разобрать ключевые слова
+
+    public static void main(String[] args) {
+        /* HW 1*/
+        hw1.printResult();
+    }
+}
