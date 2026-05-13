1cd091c2951aa583606dfe49b89805fd4be91059
Maksim <maxyarmoluk@gmail.com>
2026-05-13 16:24:41 +0300

    убрал out, переименовал пакеты (ошибки)

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
+import by.Homework.lessons.HW1.task1;
+
 public class Main {
-    private static final HW1 hw1 = new HW1();   //разобрать ключевые слова
+    private static final task1 task1 = new task1();   //разобрать ключевые слова
 
     public static void main(String[] args) {
         /* HW 1*/
-        hw1.printResult();
+        task1.printResultTask1();
     }
 }
