c082fca4816a124afbb92dc0a1c5cd462fe79e4b
Maksim <59695743+ErizoSmile135@users.noreply.github.com>
2026-05-13 16:37:49 +0300

    Delete src/by directory

--- src/by/homewirk/lessons/task1/HW1.java
+++
@@ -1,9 +0,0 @@
-package by.homewirk.lessons.task1;
-
-public class HW1 {
-    private static String strOut = "Ярмолюк. 22.04.2026 19:46";
-
-    public void printResult(){
-        System.out.print(strOut);
-    }
-}
