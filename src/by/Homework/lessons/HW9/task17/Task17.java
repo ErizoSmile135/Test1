46bac8a451155ffab0e9c176509851e69cf95952
Maksim <maxyarmoluk@gmail.com>
2026-07-18 15:08:28 +0300

    Подготовка

---
+++ src/by/Homework/lessons/HW9/task17/Task17.java
@@ -0,0 +1,27 @@
+package by.Homework.lessons.HW9.task17;
+
+public class Task17 {
+}
+
+
+/*  Домашнее задание №17
+    Гербер Шилдт. Полное руководство. Java 12 -ое издание. Страницы: с 367-401, 813-868
+    Дан текст: The Rostóvs knew everybody in Moscow. The old count had money enough that year, as all his estates had been remortgaged, and so Nicholas, acquiring a trotter of his own, very stylish riding breeches of the latest cut, such as no one else yet had in Moscow, and boots of the latest fashion, with extremely pointed toes and small silver spurs, passed his time very gaily. After a short period of adapting himself to the old conditions of life, Nicholas found it very pleasant to be at home again. He felt that he had grown up and matured very much. His despair at failing in a Scripture examination, his borrowing money from Gavríl to pay a sleigh driver, his kissing Sónya on the sly—he now recalled all this as childishness he had left immeasurably behind. Now he was a lieutenant of hussars, in a jacket laced with silver, and wearing the Cross of St. George, awarded to soldiers for bravery in action, and in the company of well-known, elderly, and respected racing men was training a trotter of his own for a race. He knew a lady on one of the boulevards whom he visited of an evening. He led the mazurka at the Arkhárovs’ ball, talked about the war with Field Marshal Kámenski, visited the English Club, and was on intimate terms with a colonel of forty to whom Denísov had introduced him.
+    1.1 Записать его в файл,  прописав относительный путь. Реализуйте соответствующий метод в классе Main.
+    1.2 Записать его в файл прописав абсолютный путь. Реализуйте соответствующий метод в классе Main
+    1.3 Вычитать текст из первого файла. Реализуйте соответствующий метод в классе Main
+    1.4 Вычитать текст из второго файла Реализуйте соответствующий метод в классе Main
+    2.1 Создать объект Автомобиль с полями (Марка автомобиля, максимальная скорость, страна производства).
+        Переопределите метод toString(), equals(), hashcode(), гетеры и сетеры (генерируйте с помощью идеи).
+        В классе Main создать объект автомобиль. Выполнить сериализацию объекта. Реализуйте соответствующий метод в классе Main
+    2.2 Выполнить десериализацию автомобиля и вывести его в консоли. Реализуйте соответствующий метод в классе Main
+
+    Сроки выполнения 12.07.2026 Ссылку на пул реквест бросать мне в личку в телеграмме
+    Вопросы для проработки:
+    1. Назовите основные классы для потоков ввода и вывода
+    2. Чем отличаются ключевые слова final, finally, finalize?
+    3. Что такое сериализация?
+    4. Ключевое слово transient, что обозначает?
+    5. Назовите основные классы для чтения  записи текста в файл.
+    6. Что такое try c ресурсами?
+ */
